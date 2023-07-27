9db8004cd82f315835bef7f9328337f8bd5142b8
NhatPham <nhatpham@192.168.2.18>
2023-07-27 21:24:27 +0700

    UI CoinItem

---
+++ src/components/CoinItem/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import styles from "./styles";
+const ConinItem = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Nhat dang lam gi do</Text>
+    </View>
+  );
+};
+export default ConinItem;
