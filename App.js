faf2615c0e2720b8bd7b689c9ad61e04e3bfe4a5
Pham Vuong Hoang Nhat <83819988+hoangnhat1997@users.noreply.github.com>
2023-07-28 20:50:50 +0700

    UI CoinItem (#1)
    * UI CoinItem
    
    * UI App
    
    ---------
    
    Co-authored-by: NhatPham <nhatpham@192.168.2.18>

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View, ActivityIndicator } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import Navigation from "./src/navigation";
+import WatchlistProvider from "./src/Contexts/WatchlistContext";
+import { RecoilRoot } from "recoil";
+import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+    DroidSans: require("./assets/fonts/DroidSans.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator size={"large"} />;
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer
+      theme={{
+        colors: {
+          background: "#121212",
+        },
+      }}
+    >
+      <RecoilRoot>
+        <WatchlistProvider>
+          <GestureHandlerRootView style={styles.container}>
+            <Navigation />
+            <StatusBar style="light" />
+          </GestureHandlerRootView>
+        </WatchlistProvider>
+      </RecoilRoot>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#121212",
+    paddingTop: 50,
   },
 });
